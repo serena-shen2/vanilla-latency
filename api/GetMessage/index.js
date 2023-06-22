e269a7b4acaa378f4de094400fd263550420e15d
Serena Shen <108768434+serena-shen2@users.noreply.github.com>
2023-06-22 15:39:07 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,22 +1,24 @@
 module.exports = async function (context, req) {
   const url = 'https://mango-pond-0a4fd7900.3.azurestaticapps.net'; // Replace with the URL of the website you want to test
-  
+
   let totalResponseTime = 0;
-  
+
   for (let i = 0; i < 100; i++) {
     const startTime = Date.now();
-    fetch(url)
+    await fetch(url)
       .then(() => {
         const endTime = Date.now();
         const responseTime = endTime - startTime;
         totalResponseTime += responseTime;
-        if (i === 99) {
-          const averageResponseTime = totalResponseTime / 100;
-          document.getElementById('response-time').innerText = `Average response time: ${averageResponseTime}ms`;
-        }
       })
       .catch((err) => {
         console.error(err);
       });
   }
+
+  const averageResponseTime = totalResponseTime / 100;
+
+  context.res = {
+    body: { averageResponseTime },
+  };
 };
