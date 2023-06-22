0cf2e79d27b7c5a03edb2951d06fdf25b3e45adb
Serena Shen <108768434+serena-shen2@users.noreply.github.com>
2023-06-22 16:05:13 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -2,8 +2,9 @@ module.exports = async function (context, req) {
   const url = 'https://mango-pond-0a4fd7900.3.azurestaticapps.net'; // Replace with the URL of the website you want to test
 
   let totalResponseTime = 0;
+  const now = new Date();
 
-  const startTime = Date.now();
+  const startTime = new Date.now();
   await fetch(url)
     .then(() => {
       const endTime = Date.now();
