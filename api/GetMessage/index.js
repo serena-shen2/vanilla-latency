b62352e56b6eaf0bc34d63fbb4d786bdf2fca691
Serena Shen <108768434+serena-shen2@users.noreply.github.com>
2023-06-22 17:28:18 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -2,9 +2,13 @@ module.exports = async function (context, req) {
   const url =  'https://icy-grass-026a80e10.3.azurestaticapps.net/';
   let totalMilliseconds = 0;
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 1; i++) {
     const start = Date.now();
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    });
     const content = await response.text();
     const end = Date.now();
     totalMilliseconds += end - start;
