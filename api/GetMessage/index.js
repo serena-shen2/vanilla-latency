3fc5a3cc4b12284e92bb142c80fb6c951fda6614
Serena Shen <108768434+serena-shen2@users.noreply.github.com>
2023-06-22 17:37:31 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
   const url =  'https://icy-grass-026a80e10.3.azurestaticapps.net/';
   let totalMilliseconds = 0;
 
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < 10; i++) {
     const start = Date.now();
     const response = await fetch(url, {
       headers: {
@@ -15,7 +15,7 @@ module.exports = async function (context, req) {
     totalMilliseconds += end - start;
   }
 
-  const averageMilliseconds = totalMilliseconds / 100;
+  const averageMilliseconds = totalMilliseconds / 10;
   context.res = {
     body: { text: `Average response time: ${averageMilliseconds} ms` },
   };
