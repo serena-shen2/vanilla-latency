d89210c69ca8c85f6b4661e988f9c584f2365bf5
Serena Shen <108768434+serena-shen2@users.noreply.github.com>
2023-06-22 17:18:19 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,17 @@
 module.exports = async function (context, req) {
-fetch("https://jsonplaceholder.typicode.com/posts/1")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-};
+  const url =  'https://icy-grass-026a80e10.3.azurestaticapps.net/';
+  let totalMilliseconds = 0;
 
+  for (let i = 0; i < 100; i++) {
+    const start = Date.now();
+    const response = await fetch(url);
+    const content = await response.text();
+    const end = Date.now();
+    totalMilliseconds += end - start;
+  }
+
+  const averageMilliseconds = totalMilliseconds / 100;
+  context.res = {
+    body: { text: `Average response time: ${averageMilliseconds} ms` },
+  };
+}
