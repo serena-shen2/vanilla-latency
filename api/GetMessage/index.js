6979c58a5996a2257212cb75ee612a2d1396ef55
Serena Shen <108768434+serena-shen2@users.noreply.github.com>
2023-06-22 17:13:05 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,15 +1,6 @@
 module.exports = async function (context, req) {
-  const edge = require('edge-js');
-
-const myScript = edge.func({
-  assemblyFile: 'myScript.dll',
-  typeName: 'Startup',
-  methodName: 'Invoke'
-});
-
-myScript({ name: 'John' }, (error, result) => {
-  if (error) throw error;
-  console.log(result);
-});
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
 };
 
