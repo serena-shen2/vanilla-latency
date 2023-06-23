f6be8df4d2e5ad4d8cd27fdadd9af2a086b20b30
Serena Shen <108768434+serena-shen2@users.noreply.github.com>
2023-06-23 11:56:21 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -5,15 +5,19 @@ function testLatency(url, numAttempts) {
   let totalResponseTime = 0;
   for (let i = 0; i < numAttempts; i++) {
     const startTime = Date.now();
-    fetch(url, (res) => {
-      const endTime = Date.now();
-      const responseTime = endTime - startTime;
-      totalResponseTime += responseTime;
-      if (i === numAttempts - 1) {
-        const averageResponseTime = totalResponseTime / numAttempts;
-        averageResponse = averageResponseTime;
-      }
-    });
+    fetch(url)
+      .then((res) => {
+        const endTime = Date.now();
+        const responseTime = endTime - startTime;
+        totalResponseTime += responseTime;
+        if (i === numAttempts - 1) {
+          averageResponse = totalResponseTime / numAttempts;
+          console.log(`Average response time: ${averageResponseTime} ms`);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 }
 
