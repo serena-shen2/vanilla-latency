80a38b7776a53e67143459efcac949398eb0d506
Serena Shen <108768434+serena-shen2@users.noreply.github.com>
2023-06-23 11:55:21 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,12 +1,11 @@
 module.exports = async function (context, req) {
-  const url =  'https://icy-grass-026a80e10.3.azurestaticapps.net/';
 const https = require('https');
 let averageResponse = 0;
 function testLatency(url, numAttempts) {
   let totalResponseTime = 0;
   for (let i = 0; i < numAttempts; i++) {
     const startTime = Date.now();
-    https.get(url, (res) => {
+    fetch(url, (res) => {
       const endTime = Date.now();
       const responseTime = endTime - startTime;
       totalResponseTime += responseTime;
@@ -18,7 +17,7 @@ function testLatency(url, numAttempts) {
   }
 }
 
-testLatency('https://example.com', 100);
+testLatency('https://icy-grass-026a80e10.3.azurestaticapps.net/', 100);
   context.res = {
     body: { text: `Average response time: ${averageResponse} ms` },
   };
