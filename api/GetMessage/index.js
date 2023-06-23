3f87ae39e9cb7447939f168fd317a38b190c86cd
Serena Shen <108768434+serena-shen2@users.noreply.github.com>
2023-06-23 12:06:45 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,25 +1,37 @@
-module.exports = async function (context, req) {
 const https = require('https');
 
-  function testLatency(url, numAttempts) {
-  let totalResponseTime = 0;
-  for (let i = 0; i < numAttempts; i++) {
-    const startTime = Date.now();
-    https.get(url, (res) => {
-      const endTime = Date.now();
-      const responseTime = endTime - startTime;
-      totalResponseTime += responseTime;
-      if (i === numAttempts - 1) {
-        const averageResponse = totalResponseTime / numAttempts;
+function testLatency(url, numAttempts) {
+  return new Promise((resolve, reject) => {
+    let totalResponseTime = 0;
+    for (let i = 0; i < numAttempts; i++) {
+      const startTime = Date.now();
+      https.get(url, (res) => {
+        const endTime = Date.now();
+        const responseTime = endTime - startTime;
+        totalResponseTime += responseTime;
+        if (i === numAttempts - 1) {
+          const averageResponseTime = totalResponseTime / numAttempts;
+          resolve(averageResponseTime);
+        }
+      }).on('error', (err) => {
+        reject(err);
+      });
+    }
+  });
+}
 
-      }
-    });
+module.exports = async function (context, req) {
+  const url = 'https://icy-grass-026a80e10.3.azurestaticapps.net/';
+  const numAttempts = 100;
+  try {
+    const averageResponseTime = await testLatency(url, numAttempts);
+    return {
+      body: `Average response time: ${averageResponseTime} ms`,
+    };
+  } catch (err) {
+    return {
+      status: 500,
+      body: err.message,
+    };
   }
-}
-
-testLatency('https://icy-grass-026a80e10.3.azurestaticapps.net/', 100);
-
-            context.res = {
-            body: { text: `Average response time: ${averageResponse} ms` },
-          };
-}
+};
